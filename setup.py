68f8007bb18cf7604807b8fdda53a77cf4858d0a
vit0r <vit0r@users.noreply.github.com>
2019-08-06 00:04:50 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     packages=find_packages(exclude='tests'),
-    tests_require=['pytest', 'werkzeug', 'click'],
-    install_requires=['click', 'werkzeug'],
+    tests_require=['pytest==5.0.1', 'werkzeug==0.15.5', 'click==7.0'],
+    install_requires=['werkzeug==0.15.5', 'click==7.0'],
     entry_points={
         'console_scripts': [
             'cwmock=cwmock.__main__:main'
