69bb3faa887d955409cc77b99e38cb8c7b05f1a8
vit0r <vitornascimentoaraujo@gmail.com>
2020-06-20 20:45:55 -0300

    fix deps on setup.py

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ from setuptools import find_packages, setup
 setup(
     packages=find_packages(exclude="tests"),
     tests_require=[
-        "pytest==5.0.1",
-        "werkzeug==0.15.5",
-        "click==7.0",
-        "tinymongo==0.1.9",
+        "pytest==5.4.3",
+        "Werkzeug==1.0.1",
+        "click==7.1.2",
+        "tinydb==4.1.1",
     ],
-    install_requires=["werkzeug==0.15.5", "click==7.0", "tinymongo==0.1.9"],
-    entry_points={"console_scripts": ["dynomock=dynomock.__main__:main"],},
+    install_requires=["werkzeug==0.15.5", "click==7.1.2", "tinydb==4.1.1"],
+    entry_points={"console_scripts": ["dynomock=dynomock.__main__:main"], },
 )
